86df381847e4372105c72957cee7036449b3e00a
Nick P Smith <npsmith1990@gmail.com>
2016-02-04 17:01:00 -0600

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Nicholas Smith on 2/4/16.
+//  Copyright © 2016 Nicholas Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
